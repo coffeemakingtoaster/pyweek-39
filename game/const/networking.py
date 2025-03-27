07ddc51793bf3e28063ac8ded8ce941e00afe0d1
coffeemakingtoaster <coffeemakingtoaster@gmail.com>
2025-03-27 21:27:25 +0100

    trigger build

--- game/const/networking.py
+++ game/const/networking.py
@@ -1,5 +1,5 @@
-HOST = "localhost:3000"
-#HOST = "pyweek-39.ssh-coffee.dev"
+#HOST = "localhost:3000"
+HOST = "pyweek-39.ssh-coffee.dev"
 
 TIME_BETWEEN_PACKAGES_IN_S = 0.05
 POSITION_DIFF_THRESHOLD = 0.1
