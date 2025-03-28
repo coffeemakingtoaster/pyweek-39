b6e47fbf765939c477e9c5fdcbf60c6892854c11
coffemakingtoaster <mh340@hdm-stuttgart.de>
2025-03-28 12:00:34 +0100

    feat(gui): update game end screen

--- game/gui/game_end.py
+++ game/gui/game_end.py
@@ -14,20 +14,43 @@ class GameEnd(GuiBase):
 
         buttonImages = loader.loadTexture("assets/textures/button_bg.png"),
         font = loader.loadFont("assets/fonts/the_last_shuriken.ttf")
+        
+        backdrop = DirectFrame(
+            frameSize=(-0.8, 0.8, -0.8, 0.8),
+            pos=(0, 0, 0.3), 
+            frameColor = (1,1,1,1),
+            frameTexture = "assets/textures/end_game_backdrop.png",
+        )
+        backdrop.setTransparency(TransparencyAttrib.MAlpha)
+        self.ui_elements.append(backdrop)
+
 
         state_image = loader.loadTexture("assets/textures/victory.png")
+        text = "victory"
 
         if not self.is_victory:
             state_image = loader.loadTexture("assets/textures/defeat.png")
+            text = "defeat"
 
         state_indicator = DirectFrame(
             frameSize=(-0.8, 0.8, -0.8, 0.8),
-            pos=(0, 0, 0), 
+            pos=(0, 0, 0.3), 
             frameColor = (1,1,1,1),
             frameTexture = state_image,
         )
         state_indicator.setTransparency(TransparencyAttrib.MAlpha)
         self.ui_elements.append(state_indicator)
+        
+        state_text = DirectLabel(
+            scale=0.07,
+            pos=(0,0,-0.3),
+            text_fg=(TEXT_PRIMARY_COLOR) if self.is_victory else (1,0,0,1),
+            text_font=font,
+            text=text,
+            frameColor = (1,1,1,0)
+        )
+        state_text.setTransparency(TransparencyAttrib.MAlpha)
+        self.ui_elements.append(state_text)
 
         main_menu_button = DirectButton(text=("Main Menu"),
                     pos=(0,0,-0.85), 
@@ -47,3 +70,4 @@ class GameEnd(GuiBase):
 
     def __return_to_main_menu(self):
         messenger.send(GUI_RETURN_EVENT)
+
